80b6c6c56135d8592761f8b7b81853a4216951b4
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2022-10-26 20:42:55 +0300

    added products list array

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const productContainer = document.getElementById("products-container");
+
+const productsList  = ["Black Tea", "Green Tea"];
+
+
 const blackTea = document.createElement("div");
 const greenTea = document.createElement("div");
 
-blackTea.innerHTML = "Black Tea";
-greenTea.innerHTML = "Green Tea";
+blackTea.innerHTML = productsList[0];
+greenTea.innerHTML = productsList[1];
+
 
 blackTea.classList.add("product");
 greenTea.classList.add("product");
