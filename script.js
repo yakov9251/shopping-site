2a89aff9dacae5012a234e726054d19207e54796
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2022-10-26 19:56:57 +0300

    added dynamic html in js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello tea shop!');
+const productContainer = document.getElementById("products-container");
+const blackTea = document.createElement("div");
+const greenTea = document.createElement("div");
+
+blackTea.innerHTML = "Black Tea";
+greenTea.innerHTML = "Green Tea";
+
+blackTea.classList.add("product");
+greenTea.classList.add("product");
+
+productContainer.appendChild(blackTea);
+productContainer.appendChild(greenTea);
