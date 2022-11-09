6d4ad819cb5744a2adc8e7aab5536dbd3569d1d9
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2022-11-09 20:10:08 +0200

    added product object and created main layout

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const productContainer = document.getElementById("products-container");
-
-const productsList  = ["Black Tea", "Coffee", "Green Tea", "Milk"];
-
-for (let i=0;i<productsList.length;i++) {
-    const product = document.createElement("div");
-    product.innerHTML = productsList[i];
-    product.classList.add("product");
-    productContainer.appendChild(product);
+debugger;
+const product = {
+    name: 'Black Tea',
+    amount: 33,
+    price: 20
 }
 
-console.log('done');
+//create main element
+const productDIV = document.createElement("div");
+productDIV.classList.add("product");
+
+//create name container
+const productNameDIV = document.createElement("div");
+productNameDIV.innerHTML = product.name;
+productDIV.appendChild(productNameDIV);
+
+//create amount container
+const productAmountDIV = document.createElement("div");
+productAmountDIV.innerHTML = "Amount: " + product.amount;
+productDIV.appendChild(productAmountDIV);
+
+
+productContainer.appendChild(productDIV);
+
+
+
+
+
+// const productsList  = ["Black Tea", "Coffee", "Green Tea", "Milk"];
+
+// for (let i=0;i<productsList.length;i++) {
+//     const product = document.createElement("div");
+//     product.innerHTML = productsList[i];
+//     product.classList.add("product");
+//     productContainer.appendChild(product);
+// }
+
+// console.log('done');
 
