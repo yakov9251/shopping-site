23b6fa8a3336fe347e557eff8f94d724b089773a
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2022-11-09 20:33:10 +0200

    added products object array

--- script.js
+++ script.js
@@ -1,44 +1,45 @@
 const productContainer = document.getElementById("products-container");
-debugger;
-const product = {
+
+const productsArray = [];
+
+productsArray.push({
     name: 'Black Tea',
-    amount: 33,
-    price: 20,
-    image : {
-        src: "http://www.mysite.com/myimage.jpg",
-        alt: "black tea image"
-    }
+    amount: 99,
+    price: 20
+});
+
+productsArray.push({
+    name: 'Coffee',
+    amount: 2,
+    price: 21
+});
+
+productsArray.push({
+    name: 'Milk',
+    amount: 100,
+    price: 3
+});
+
+
+productsArray.push({
+    name: 'Green Tea',
+    amount: 29713,
+    price: 1
+});
+
+for(let i=0; i<productsArray.length;i++) {
+
+    const productDIV = document.createElement("div");
+    productDIV.classList.add("product");
+
+    const productNameDIV = document.createElement("div");
+    productNameDIV.innerHTML = productsArray[i].name;
+    productDIV.appendChild(productNameDIV);
+
+    const productAmountDIV = document.createElement("div");
+    productAmountDIV.innerHTML = "Amount: " + productsArray[i].amount;
+    productDIV.appendChild(productAmountDIV);
+
+    productContainer.appendChild(productDIV);
+
 }
-
-//create main element
-const productDIV = document.createElement("div");
-productDIV.classList.add("product");
-
-//create name container
-const productNameDIV = document.createElement("div");
-productNameDIV.innerHTML = product.name;
-productDIV.appendChild(productNameDIV);
-
-//create amount container
-const productAmountDIV = document.createElement("div");
-productAmountDIV.innerHTML = "Amount: " + product.amount;
-productDIV.appendChild(productAmountDIV);
-
-
-productContainer.appendChild(productDIV);
-
-
-
-
-
-// const productsList  = ["Black Tea", "Coffee", "Green Tea", "Milk"];
-
-// for (let i=0;i<productsList.length;i++) {
-//     const product = document.createElement("div");
-//     product.innerHTML = productsList[i];
-//     product.classList.add("product");
-//     productContainer.appendChild(product);
-// }
-
-// console.log('done');
-
